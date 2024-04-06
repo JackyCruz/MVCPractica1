0356f086aa269bbc4dc45f394962b1a4c0c14374
JackyCruz <jacquelinne.cruz@catolica.edu.sv>
2024-04-05 23:19:03 -0600

    Agregar archivos de proyecto.

---
+++ MVCPractica1/Models/equiposDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVCPractica1.Models
+{
+	public class equiposDbContext : DbContext
+	{
+		public equiposDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<marcas> marcas { get; set; }
+		public DbSet<tipo_equipo> tipo_Equipo { get; set; }
+		public DbSet<estados_equipo> estados_equipo { get; set; }
+		public DbSet<equipos> equipos { get; set; }
+	}
+}
